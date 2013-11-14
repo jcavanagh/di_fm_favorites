d5ac463ae05d70d51c163fa5e721a8816c680803
Joe Cavanagh <joe.cavanagh@corvisa.com>
2013-11-14 16:23:31 -0600

    Extension manifest, angular, bootstrap, etc

---
+++ js/app.js
@@ -0,0 +1,27 @@
+angular.module('di_fm_favorites', [
+    'ngRoute',
+    'di_fm_favorites.directives'
+]).config(function($routeProvider) {
+    //Routes!
+    $routeProvider.when('/', {
+        templateUrl: '/views/_favorites.html'
+    });
+
+    $routeProvider.when('/login', {
+        templateUrl: '/views/_login.html'
+    });
+}).run(function($rootScope, $location) {
+    //http://stackoverflow.com/questions/11541695/angular-js-redirecting-to-a-certain-route-based-on-condition
+    // register listener to watch route changes
+    $rootScope.$on( "$routeChangeStart", function(event, next, current) {
+        if ( $rootScope.user == null ) {
+            // no logged user, we should be going to #login
+            if ( next.templateUrl == '/views/_login.html' ) {
+                // already going to #login, no redirect needed
+            } else {
+                // not going to #login, we should redirect now
+                $location.path( '/login' );
+            }
+        }
+    });
+})
---
+++ js/directives.js
@@ -0,0 +1,1 @@
+angular.module('di_fm_favorites.directives', []);
